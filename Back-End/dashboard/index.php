c33d3ca95f2102bb2e4f6a45febe87483857ed12
Amr Shalaby <amr.m7md.shalaby@gmail.com>
2022-10-06 18:48:19 +0200

    dashboard

---
+++ Back-End/dashboard/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include_once "layout/head.php"
+    ?>
+</head>
+<body>
+<?php
+    include_once "layout/navBar.php";
+    include_once "layout/sideBar.php";
+    include_once "layout/footer.php"
+    ?>
+</body>
+</html>
